de2776b530010490167b2052dca4e88c12d29bac
Manuel Alted <kyrati@hotmail.com>
2019-10-04 20:25:12 +0200

    Crear un hola y adios mundo

---
+++ AdiosMundo.java
@@ -0,0 +1,15 @@
+public class AdiosMundo{
+	public static void main(String[] args){
+
+
+
+
+
+
+	}
+
+
+
+
+
+}
---
+++ HolaMundo.java
@@ -0,0 +1,15 @@
+public class HolaMundo{
+	public static void main(String[] args){
+
+
+
+
+		
+	}
+
+
+
+
+
+
+}
